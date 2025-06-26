37b8f8cc5603a8d9539e28b4c4e73e6582b6d4c9
mohini chauhan 22SCSE2030513 <you@example.com>
2025-06-26 16:02:34 +0530

    Added Mapview and updated MapCard

--- src/components/MapCard.jsx
+++ src/components/MapCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import  Card  from "./ui/Card";
 import  CardContent  from "./ui/CardContent";
+import Mapview from './Mapview';
 
 const MapCard= ()=> {
   return (
-    <Card className="lg:col-span-1 h-[300px] w-[410px] bg-[#D9FFCE]]">
-      <CardContent className="w-full  h-[300px] aspect-[4/3] sm:aspect-[5/3] md:aspect-[4/2] lg:aspect-[4/2] overflow-hiddenitems-center">
-        <img src="/images/image (3).png" alt="Map" className="w-full h-full object-cover rounded-lg" />
+    <Card className="lg:col-span-1 h-[300px] w-[410px]  bg-[#D9FFCE]]">
+      <CardContent className=" h-[300px] aspect-[4/3] sm:aspect-[5/3] md:aspect-[4/2] lg:aspect-[4/2] overflow-hiddenitems-center">
+       <Mapview />
       </CardContent>
     </Card>
   );
---
+++ src/components/Mapview.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+function Mapview() {
+  return (
+    <>
+        <MapContainer
+      center={[16.705, 74.2433]} // Kolhapur coordinates
+      zoom={10}
+      scrollWheelZoom={false}
+      className="h-[300px] w-[410px]  rounded-lg"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={[16.705, 74.2433]}>
+        <Popup>
+          Kolhapur Plant
+        </Popup>
+      </Marker>
+    </MapContainer>
+  
+    </>
+  )
+}
+
+export default Mapview
